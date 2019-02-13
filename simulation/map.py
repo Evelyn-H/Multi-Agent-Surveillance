5e509ae0dc4f59f53b0d08accff7d12abb8f3944
Evelyn_H <manu.hobert@gmail.com>
2019-02-13 19:25:54 +0100

    Added minor implementations

--- simulation/map.py
+++ simulation/map.py
@@ -17,15 +17,15 @@ class Tower:
 class Map:
     """Stores info about the map"""
 
-    def __init__(self, size: Tuple[int, int]) -> None:
+    def __init__(self, size: Tuple[int, int], targets: List[Position]=None, structures=None, towers: List[Tower]=None) -> None:
         # metadata about the map
-        self._size: Tuple[int, int] = size
-        self._targets: List[Position] = [...]
+        self.size: Tuple[int, int] = size
+        self.targets: List[Position] = targets if targets else []
 
         # info about tiles
         self._vision_modifier: List[List[float]] = [[1.0] * size[1]] * size[0]
 
         # structures and stuff on the map
         # TODO: decide on structure implementation (see github issue #3)
-        self._structures = [...]
-        self._towers: List[Tower] = [...]
+        self.structures = structures if structures else []
+        self.towers: List[Tower] = towers if towers else []
